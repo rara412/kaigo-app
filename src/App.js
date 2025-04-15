47d91c57e274326cc78bce55bb97d5d38fc7300c
unknown <cantik.kaigo.s@jcom.home.ne.jp>
2025-04-15 17:30:07 +0900

    クリックしてメッセージ表示のボタンを追加

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const handleClick = () => {
+    setMessage('ボタンがクリックされました！');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>こんにちは、訪問介護アプリへようこそ！</h1>
         <p>これは自分で作って公開したReactアプリです。</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* 👇 ここが新しく追加する部分 */}
+        <button onClick={handleClick}>クリックしてメッセージ表示</button>
+        <p>{message}</p>
       </header>
     </div>
   );
